ca861dcac87826787893697fc345184176210eca
ivandjou <djouivan03@gmail.com>
2025-06-02 19:12:19 +0000

    while_loops

---
+++ while_loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int array[] = {1, 7, 4, 5, 9, 3, 5, 11, 6, 3, 4};
+    int i = 0;
+
+    while (i < 11) {
+        printf("%d\n", array[i]);
+        i++;
+    }
+
+    return 0;
+}
+
