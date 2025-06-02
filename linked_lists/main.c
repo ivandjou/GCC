3edb1e08900cc30aef36382e97e712f90b8585ef
ivandjou <djouivan03@gmail.com>
2025-06-02 21:19:28 +0000

    linked_lists

---
+++ linked_lists/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Déclaration + définition de la fonction factorial */
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    /* testing code */
+    printf("0! = %i\n", factorial(0));
+    printf("1! = %i\n", factorial(1));
+    printf("3! = %i\n", factorial(3));
+    printf("5! = %i\n", factorial(5));
+
+    return 0;
+}
+
